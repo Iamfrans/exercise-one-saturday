0b45ce3198dd9971e1286e1393258b19b6fe0ad7
Iamfrans <1323544@senati.pe>
2023-03-25 08:21:51 -0500

    Print

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Alumno
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Morning, Morning, Morning ");
+        System.out.println("       Hello, have a good day");
+        System.out.println("                oh! Thank you");
+        System.out.println("                                 ");
+        System.out.println("                You are great");
+        System.out.println("        This is my pleasure");
+        System.out.println("No,... You are welcome");
+    }
+    
+}
